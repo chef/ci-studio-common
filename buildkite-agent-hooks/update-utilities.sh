a6cfbe686737bdfc150a82717f4e2e3ff29053fc
Tom Duffield <tom@chef.io>
2018-11-05 15:09:56 -0600

    Install/upgrade Expeditor CLI as part of normal routine
    Signed-off-by: Tom Duffield <tom@chef.io>

--- buildkite-agent-hooks/update-utilities.sh
+++ buildkite-agent-hooks/update-utilities.sh
@@ -4,3 +4,7 @@ sudo ci-studio-common-util update
 
 echo "Updating 'hab'"
 sudo install-habitat
+
+echo "Updating 'expeditor' CLI"
+hab pkg install chef-es/expeditor-ruby
+hab pkg binlink --force chef-es/expeditor-ruby expeditor

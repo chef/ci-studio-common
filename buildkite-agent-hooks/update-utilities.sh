67858b01ba1937139bfaa5fd883eadf0d0bdf3c7
Jeremiah Snapp <jeremiah@chef.io>
2018-12-13 13:09:56 -0500

    Only install habitat and run habitat commands on supported architectures
    Signed-off-by: Jeremiah Snapp <jeremiah@chef.io>

--- buildkite-agent-hooks/update-utilities.sh
+++ buildkite-agent-hooks/update-utilities.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
+
+habitat_supported_platform() {
+  local habitat_supported_archs_regex='.*x86_64|amd64.*'
+  local platform="linux"
+  local uname="$(uname -sm | awk '{print tolower($0)}')"
+
+  case "$uname" in
+    *"mac os x"* | *darwin*) platform="darwin" ;;
+    *"freebsd"*) platform="freebsd" ;;
+    *) platform="linux" ;;
+  esac
+
+  [[ ($platform =~ darwin|linux) && ($uname =~ $habitat_supported_archs_regex) ]]
+}
+
 echo "Updating 'ci-studio-common'"
 sudo ci-studio-common-util update
 
-echo "Updating 'hab'"
-sudo install-habitat
+if habitat_supported_platform; then
+  echo "Updating 'hab'"
+  sudo install-habitat
 
-echo "Updating 'expeditor' CLI"
-hab pkg install chef-es/expeditor-ruby
+  echo "Updating 'expeditor' CLI"
+  hab pkg install chef-es/expeditor-ruby
+fi
